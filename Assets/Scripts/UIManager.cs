2bd26fde9efdc42f1a1eb3ac6ebbdf4625f1d846
JesusAyalaMatarin <jexuxayala@gmail.com>
2020-06-18 20:36:10 +0200

    Merge branch 'jesus'

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -41,6 +41,8 @@ public class UIManager : MonoBehaviour
 
     [Header("Nombre Color")] [SerializeField] private GameObject nombreColor;
 
+    public PolePositionManager m_polePositionManager = FindObjectOfType<PolePositionManager>();
+
     [SerializeField] private Button buttonAzul;
     [SerializeField] private Button buttonMorado;
     [SerializeField] private Button buttonVerde;
@@ -82,6 +84,8 @@ public class UIManager : MonoBehaviour
 
     private void Metododebarrera()
     {
+        m_polePositionManager.metodoBarrera(); 
+
         if (host == 1)
         {
             m_NetworkManager.StartHost();
@@ -89,6 +93,7 @@ public class UIManager : MonoBehaviour
         }
         else
         {
+
             m_NetworkManager.StartClient();
             ActivateInGameHUD();
         }
